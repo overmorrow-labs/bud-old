c6f44a56f037df1cf072374437681b6ed94848eb
shimorojune <rohitrajnovember@gmail.com>
2025-04-15 18:04:33 +0530

    chore: Install deps, setup tailwind and shadcn config

--- src/core/Agent.ts
+++ src/core/Agent.ts
@@ -1,35 +1,32 @@
 // core/agent-factory.ts
 
-import { AgentConfig, } from './types';
-
-
+import { AgentConfig } from "./types";
 
 export class Agent {
-    // statics
-    static getInstance(instanceId: string, config: AgentConfig) {
-        return new Agent(instanceId, config)
-    }
+  // statics
+  static getInstance(instanceId: string, config: AgentConfig) {
+    return new Agent(instanceId, config);
+  }
 
-    // main
-    private instanceId: string;
+  // main
+  private instanceId: string;
 
-    private config: AgentConfig;
+  private config: AgentConfig;
 
-    private constructor(instanceId: string, config: AgentConfig) {
-        this.instanceId = instanceId
-        this.config = config;
-    }
+  private constructor(instanceId: string, config: AgentConfig) {
+    this.instanceId = instanceId;
+    this.config = config;
+  }
 
-    getInstanceId() {
-        return this.instanceId;
-    }
+  getInstanceId() {
+    return this.instanceId;
+  }
 
-    getConfig() {
-        return this.config;
-    }
+  getConfig() {
+    return this.config;
+  }
 
-    async ask(input: string): Promise<string> {
-        return `[${this.instanceId}] ${this.config.context} → ${input}`;
-    }
-
+  async ask(input: string): Promise<string> {
+    return `[${this.instanceId}] ${this.config.context} → ${input}`;
+  }
 }
